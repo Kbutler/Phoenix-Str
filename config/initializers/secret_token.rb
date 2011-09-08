76212a3aecc86eb386a39abf2e710d5f7bce14c6
Keith Butler <mail_keithbu@yahoo.co.uk>
2011-09-08 11:29:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhoenixStr::Application.config.secret_token = '062357f09331ebd1813c8359410b1d31181369da0178e22d32e4f92069aa5c96a5b5ca269939983a3a85fa9a545b71b52fc665e9f2bafdc093b80ce971ec5da3'

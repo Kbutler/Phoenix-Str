1a57984cac9588e62c9df5461e15a1882ee1b9c3
Keith Butler <mail_keithbu@yahoo.co.uk>
2011-09-09 12:11:40 +0100

    Adding files

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,15 +1,23 @@
 class PageController < ApplicationController
   http_basic_authenticate_with :name => "phoenix", :password => "phoenix"
   def page1
+    @title = "Page 1"
+    @header_img = "header_home.jpg"
   end
 
   def page2
+    @title = "Page 2"
+    @header_img = "header_kurse.jpg"
   end
 
   def page3
+    @title = "Page 3"
+    @header_img = "header_kentenich.jpg"
   end
 
   def page4
+    @title = "Page 4"
+    @header_img = "header_praxisteam.jpg"
   end
 
 end

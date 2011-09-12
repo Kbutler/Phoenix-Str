07f7ec20b2067c86fd09be5bf1cb741c843e2db9
Keith Butler <mail_keithbu@yahoo.co.uk>
2011-09-12 09:58:43 +0100

    Style growing

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,23 +1,27 @@
 class PageController < ApplicationController
   http_basic_authenticate_with :name => "phoenix", :password => "phoenix"
   def page1
-    @title = "Page 1"
-    @header_img = "header_home.jpg"
+    @title         = "Home"
+    @header_img    = "header_home.jpg"
+    @wallpaper_img = "wallpaper_home.png"
   end
 
   def page2
-    @title = "Page 2"
-    @header_img = "header_kurse.jpg"
+    @title         = "Kurse"
+    @header_img    = "header_kurse.jpg"
+    @wallpaper_img = "wallpaper_kurse.png"
   end
 
   def page3
-    @title = "Page 3"
-    @header_img = "header_kentenich.jpg"
+    @title         = "Kentenich"
+    @header_img    = "header_kentenich.jpg"
+    @wallpaper_img = "wallpaper_kentenich.png"
   end
 
   def page4
-    @title = "Page 4"
-    @header_img = "header_praxisteam.jpg"
+    @title         = "Praxisteam"
+    @header_img    = "header_praxisteam.jpg"
+    @wallpaper_img = "wallpaper_praxisteam.png"
   end
 
 end

a25995fecc26ddd49672d09c52900d1f99eeab58
Keith Butler <mail_keithbu@yahoo.co.uk>
2011-09-13 12:37:25 +0100

    Home rerouted

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ PhoenixStr::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'page#page1'
 
   # See how all your routes lay out with "rake routes"
 

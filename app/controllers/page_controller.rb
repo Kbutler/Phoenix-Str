5a7903637b1bea638e7d6bdba28d3d70a7eb62c1
Keith Butler <mail_keithbu@yahoo.co.uk>
2011-09-15 10:31:36 +0100

    extentions

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,27 +1,39 @@
 class PageController < ApplicationController
   http_basic_authenticate_with :name => "phoenix", :password => "phoenix"
+    
+  @@locallogo_img      = "logo.gif"
+  @@localfavicon_img   = 'favicon.ico'
+
   def page1
     @title         = "Home"
-    @header_img    = "header_home.jpg"
-    @wallpaper_img = "wallpaper_home.png"
+    @header_img    = "try2.jpg"
+    @wallpaper_img = "wallpaper_home.jpg"
+    @logo_img      = @@locallogo_img
+    @favicon_img   = @@localfavicon_img
   end
 
   def page2
     @title         = "Kurse"
     @header_img    = "header_kurse.jpg"
-    @wallpaper_img = "wallpaper_kurse.png"
+    @wallpaper_img = "wallpaper_kurse.jpg"
+    @logo_img      = @@locallogo_img
+    @favicon_img   = @@localfavicon_img
   end
 
   def page3
     @title         = "Kentenich"
     @header_img    = "header_kentenich.jpg"
-    @wallpaper_img = "wallpaper_kentenich.png"
+    @wallpaper_img = "wallpaper_kentenich.jpg"
+    @logo_img      = @@locallogo_img
+    @favicon_img   = @@localfavicon_img
   end
 
   def page4
     @title         = "Praxisteam"
     @header_img    = "header_praxisteam.jpg"
-    @wallpaper_img = "wallpaper_praxisteam.png"
+    @wallpaper_img = "wallpaper_praxisteam.jpg"
+    @logo_img      = @@locallogo_img
+    @favicon_img   = @@localfavicon_img
   end
 
 end

050f91841e1eadf7cd6ef5962161570ae8aa1746
Keith Butler <mail_keithbu@yahoo.co.uk>
2011-09-15 13:09:13 +0100

    Print stabalising

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,6 +3,12 @@ class PageController < ApplicationController
     
   @@locallogo_img      = "logo.gif"
   @@localfavicon_img   = 'favicon.ico'
+  @@fullName           = "Dr. Forename Surname"
+  @@address            = "Add Address Here"
+  @@jobTitleP1         = "Praxis fur Frauenheilkunde "
+  @@jobTitleP2         = "Geburtshilfe"
+  @@phoneNum           = "Tel: +11 123 456 789"
+  @@faxNum             = "Fax: +22 123 456 789"
 
   def page1
     @title         = "Home"
@@ -10,6 +16,12 @@ class PageController < ApplicationController
     @wallpaper_img = "wallpaper_home.jpg"
     @logo_img      = @@locallogo_img
     @favicon_img   = @@localfavicon_img
+    @fullName      = @@fullName
+    @address       = @@address
+    @phoneNum      = @@phoneNum
+    @faxNum        = @@faxNum
+    @jobTitleP1    = @@jobTitleP1
+    @jobTitleP2    = @@jobTitleP2
   end
 
   def page2
@@ -18,6 +30,12 @@ class PageController < ApplicationController
     @wallpaper_img = "wallpaper_kurse.jpg"
     @logo_img      = @@locallogo_img
     @favicon_img   = @@localfavicon_img
+    @fullName      = @@fullName
+    @address       = @@address
+    @phoneNum      = @@phoneNum
+    @faxNum        = @@faxNum
+    @jobTitleP1    = @@jobTitleP1
+    @jobTitleP2    = @@jobTitleP2
   end
 
   def page3
@@ -26,6 +44,12 @@ class PageController < ApplicationController
     @wallpaper_img = "wallpaper_kentenich.jpg"
     @logo_img      = @@locallogo_img
     @favicon_img   = @@localfavicon_img
+    @fullName      = @@fullName
+    @address       = @@address
+    @phoneNum      = @@phoneNum
+    @faxNum        = @@faxNum
+    @jobTitleP1    = @@jobTitleP1
+    @jobTitleP2    = @@jobTitleP2
   end
 
   def page4
@@ -34,6 +58,12 @@ class PageController < ApplicationController
     @wallpaper_img = "wallpaper_praxisteam.jpg"
     @logo_img      = @@locallogo_img
     @favicon_img   = @@localfavicon_img
+    @fullName      = @@fullName
+    @address       = @@address
+    @phoneNum      = @@phoneNum
+    @faxNum        = @@faxNum
+    @jobTitleP1    = @@jobTitleP1
+    @jobTitleP2    = @@jobTitleP2
   end
 
 end

70b661fb74dfb21e29ff60f5004b5279389a9981
Keith Butler <mail_keithbu@yahoo.co.uk>
2011-11-24 12:18:50 +0000

    debug heroku bcrypt

--- app/models/admined_login.rb
+++ app/models/admined_login.rb
@@ -5,8 +5,14 @@ class AdminedLogin < ActiveRecord::Base
     if user &&
        user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt) &&
        user.ip_hash       == BCrypt::Engine.hash_secret(ip_add, user.ip_salt)
+      puts "name = " name
+      puts "password = " password
+      puts "ip = " ip_add
       user
     else
+      puts "name = " name
+      puts "password = " password
+      puts "ip = " ip_add
       nil
     end
   end

6bf587c0d4c554576ae4f905163ab370e74b9030
Keith Butler <mail_keithbu@yahoo.co.uk>
2011-11-24 12:30:46 +0000

    log

--- app/models/admined_login.rb
+++ app/models/admined_login.rb
@@ -5,11 +5,13 @@ class AdminedLogin < ActiveRecord::Base
     if user &&
        user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt) &&
        user.ip_hash       == BCrypt::Engine.hash_secret(ip_add, user.ip_salt)
+      puts "My log message 1"
       puts name
       puts password
       puts ip_add
       user
     else
+      puts "My log message 2"
       puts name
       puts password
       puts ip_add

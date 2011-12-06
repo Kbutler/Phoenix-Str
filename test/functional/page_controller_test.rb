ed9fa3e5c55c8960ef9f4cd1a1b5ae2232cf73f7
Keith Butler <mail_keithbu@yahoo.co.uk>
2011-12-06 11:28:35 +0000

    added rake docs & more tests

--- test/functional/page_controller_test.rb
+++ test/functional/page_controller_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class PageControllerTest < ActionController::TestCase
+
   test "should get page1" do
     get :page1
+    assert_response :success
+  end
+
+  test "should get page1 en" do
+    get( :page1, {'locale' => "en"})
+    assert_response :success
+  end
+
+  test "should get page1 de" do
+    get( :page1, {'locale' => "de"})
     assert_response :success
   end
 
@@ -11,13 +22,43 @@ class PageControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get page2 en" do
+    get( :page2, {'locale' => "en"})
+    assert_response :success
+  end
+
+  test "should get page2 de" do
+    get( :page2, {'locale' => "de"})
+    assert_response :success
+  end
+
   test "should get page3" do
     get :page3
     assert_response :success
   end
 
+  test "should get page3 en" do
+    get( :page3, {'locale' => "en"})
+    assert_response :success
+  end
+
+  test "should get page3 de" do
+    get( :page3, {'locale' => "de"})
+    assert_response :success
+  end
+
   test "should get page4" do
     get :page4
+    assert_response :success
+  end
+
+  test "should get page4 en" do
+    get( :page4, {'locale' => "en"})
+    assert_response :success
+  end
+
+  test "should get page4 de" do
+    get( :page4, {'locale' => "de"})
     assert_response :success
   end
 
